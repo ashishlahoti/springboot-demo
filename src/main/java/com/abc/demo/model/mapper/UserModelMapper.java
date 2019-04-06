1999a5dccb08b9c75cd47e84f46371712f178919
lahot <lahot@aklahoti>
2019-04-06 13:41:06 +0800

    Fixed build issues

--- src/main/java/com/abc/demo/model/mapper/UserModelMapper.java
+++ src/main/java/com/abc/demo/model/mapper/UserModelMapper.java
@@ -1,22 +1,13 @@
-package com.abc.demo.dao.entity;
+package com.abc.demo.model.mapper;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.mapstruct.Mapper;
 
-import lombok.Data;
+import com.abc.demo.dao.entity.User;
+import com.abc.demo.model.request.UserRequestModel;
 
-@Entity
-@Data
-public class User {
+@Mapper(componentModel="spring")
+public interface UserModelMapper {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(nullable = false)
-	private Long id;
-	private String name;
-	private int age;
+	public User toUserEntity(UserRequestModel userRequestModel);
 	
 }
