13ea9983274c7b72e21ae310ec11c590a6577e7c
Ashish Kumar Lahoti <lahoti.ashish20@gmail.com>
2020-03-05 13:51:44 +0800

    Update SpringbootDemoApplication.java

--- src/main/java/com/abc/demo/SpringbootDemoApplication.java
+++ src/main/java/com/abc/demo/SpringbootDemoApplication.java
@@ -3,7 +3,6 @@ package com.abc.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -11,9 +10,7 @@ public class SpringbootDemoApplication {
 	
 	@Bean
 	public RestTemplate getRestTemplate() {
-		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-		clientHttpRequestFactory.setConnectTimeout(3000);
-		return new RestTemplate(clientHttpRequestFactory);
+		return new RestTemplate();
 	}
 
 	public static void main(String[] args) {
